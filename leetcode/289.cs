7fe8a3e3d461b6db4db796e62dcdd72f1f076f39
Louis (Mischa) Michael <louism@vt.edu>
2022-04-12 14:22:06 -0700

    Update 289.cs

--- leetcode/289.cs
+++ leetcode/289.cs
@@ -1,52 +1,49 @@
+// Runtime: 156 ms, faster than 76.67% of C# online submissions for Game of Life.
+// Memory Usage: 42.3 MB, less than 10.00% of C# online submissions for Game of Life.
 public class Solution {
     public void GameOfLife(int[][] board) {
-        int[][] ret = new int[board.Length][];
+        
         for(int i =0; i< board.Length; i++){
-            ret[i] = new int[board[i].Length];
             for(int j = 0; j<board[i].Length;j++){
-                ret[i][j] = this.CheckCell(board, i, j);
+                board[i][j] = this.CheckCell(board, i, j);
             }
         }
         for(int i =0; i< board.Length; i++){
             for(int j = 0; j<board[i].Length;j++){
-                board[i][j] = ret[i][j];
+                // Console.WriteLine(board[i][j]);
+                board[i][j] = board[i][j] >> 1;
+                // Console.WriteLine(board[i][j]);
             }
         }
     }
     
     public int CheckCell(int[][] board, int x, int y){
         int liveNeighbors = 0;
-        int deadNeighbors = 0;
-        for(int i = -1; i<=1;i++){
-            if(i+x < 0 || i+x >= board.Length){
-                continue;
-            }
-            for(int j = -1; j<=1;j++){
-                if(j+y < 0 || j+y >= board[i+x].Length || (j==0 && i==0)){
+        for(int i = Math.Max(0,x-1); i<=Math.Min(board.Length-1,x+1);i++){
+            for(int j = Math.Max(0,y-1); j<=Math.Min(board[i].Length-1,y+1);j++){
+                if(j==y && i==x){
                     continue;
                 }
-                if(board[x+i][y+j] == 1){
+                if(board[i][j] %2 == 1){
+                    // Console.WriteLine("liveNeighbors:"+liveNeighbors);
                     liveNeighbors++;
-                }
-                else{
-                    deadNeighbors++;
                 }
             }
         }
-        if(board[x][y] == 1){
+        if(board[x][y] %2 == 1){
             if(liveNeighbors > 3){
-                return 0;
+                return board[x][y];
             }
             if(liveNeighbors > 1){
-                return 1;
+                return board[x][y] + 2;
             }
-            return 0;
+            return board[x][y];
         }
         else{
             if(liveNeighbors == 3){
-                return 1;
+                return board[x][y] + 2;
             }
-            return 0;
+            return board[x][y];
         }
     }
 }
