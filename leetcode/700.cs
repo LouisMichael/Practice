4f908ef501c6e113d4d4b101867f547cf90d3ea2
Louis (Mischa) Michael <louism@vt.edu>
2022-04-14 10:23:31 -0700

    Create 700.cs

---
+++ leetcode/700.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if(root == null){
+            return null;
+        }
+        if(root.val == val){
+            return root;
+        }
+        if(root.val > val){
+            return this.SearchBST(root.left, val);
+        }
+        return this.SearchBST(root.right, val);
+    }
+}
