de972ca7c00aec1b25545e5a6d7cde4cd9f24016
Louis (Mischa) Michael <louism@vt.edu>
2024-12-04 05:35:44 +0000

    cleaner part1 of day 4

--- adventOfCode/2024/4/part1.py
+++ adventOfCode/2024/4/part1.py
@@ -17,13 +17,14 @@ directions = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(1,-1),(0,-1)]
 for x in range(len(grid)):
     for y in range(len(grid[x])):
         for direction in directions:
-            newX = x + direction[0]
-            newY = y + direction[1]
-            newX2 = x + direction[0]+direction[0]
-            newY2 = y + direction[1]+ direction[1]
-            newX3 = x + direction[0]+direction[0]+direction[0]
-            newY3 = y + direction[1]+ direction[1]+direction[1]
-            if inBounds(x,y) and inBounds(newX,newY) and inBounds(newX2,newY2) and inBounds(newX3,newY3):
-                if grid[x][y]+grid[newX][newY]+grid[newX2][newY2]+grid[newX3][newY3] == "XMAS":
-                    ret+=1
+            testString = ""
+            for i in range(len("XMAS")):
+                curX = x + (direction[0]*i)
+                curY = y + (direction[1]*i)
+                if inBounds(curX,curY):
+                    testString+=grid[curX][curY]
+                else:
+                    break
+            if testString == "XMAS":
+                ret+=1
 print(ret)
