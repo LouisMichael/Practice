d83112b339714db0b50e0ed3b5de3bf47efa1d7f
Louis (Mischa) Michael <louism@vt.edu>
2024-12-04 05:26:40 +0000

    prep day 5

---
+++ adventOfCode/2024/5/part1.py
@@ -0,0 +1,23 @@
+ret = 0
+
+import re
+import sys
+grid = []
+directions = [(1,0),(1,1),(0,1),(-1,1),(-1,0),(-1,-1),(1,-1),(0,-1)]
+# directions = [(1,0),(0,1),(-1,0),(0,-1)]
+
+def inBounds(x,y):
+    if x<0 or x >= len(grid) or y < 0 or y >= len(grid[x]):
+        return False
+    return True
+
+with open("input/input.txt","r", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        # grid.append(list(line))
+
+# for x in range(len(grid)):
+#     for y in range(len(grid[x])):
+#         for direction in directions:
+print(ret)
